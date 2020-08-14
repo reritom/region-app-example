73a40f4712805a3d790e26872be650214dbfb1d2
reritom <reikudjinn@gmail.com>
2020-08-14 17:58:19 +0200

    Pagination added

--- region-app/app/controllers/region_controller.py
+++ region-app/app/controllers/region_controller.py
@@ -1,13 +1,21 @@
 from flask import request, jsonify
 from app.models import Region
 from app.serialisers import RegionSerialiser
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class RegionController:
     @staticmethod
     def get_regions():
-        regions = Region.query.all()
-        print(f"There are {len(regions)} regions")
-        regions = [RegionSerialiser.serialise(region) for region in regions]
-        print(regions)
-        return jsonify(regions), 200
+        """
+        Method for retreiving the regions from the database and returning each, serialised
+        """
+        regions = (
+            Region.query.paginate(page=int(request.args["page"]), per_page=int(request.args["limit"])).items
+            if request.args.get("page") and request.args.get("limit")
+            else Region.query.all()
+        )
+        serialised_regions = [RegionSerialiser.serialise(region) for region in regions]
+        return jsonify(serialised_regions), 200
