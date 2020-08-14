183376cd585da5ad422a72d23d4e29cc230e7bbc
reritom <reikudjinn@gmail.com>
2020-08-14 14:25:45 +0200

    Base test cases added

---
+++ region-app/tests/application_test.py
@@ -0,0 +1,7 @@
+import unittest
+from app import create_app
+
+
+class ApplicationTest(unittest.TestCase):
+    def test_get_regions(self):
+        ...
---
+++ region-app/tests/populator_test.py
@@ -0,0 +1,7 @@
+import unittest
+from app import create_app
+
+
+class PopulatorTest(unittest.TestCase):
+    def test_populate_app(self):
+        ...
